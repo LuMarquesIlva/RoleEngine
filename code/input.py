fdd095098429264ae76ed3ee92b84c76d7743849
LuMarquesIlva <luismarques0504@proton.me>
2025-07-02 14:31:33 -0300

    Finished Implementing Controller Tracking and Added Mouse Input Tracking

--- code/input.py
+++ code/input.py
@@ -35,38 +35,91 @@ class Input():
 
     class Joystick:
         Joysticks = []
+        controllerCheckingIndex = 0
 
         def checkForJoysticks():
             Input.Joystick.Joysticks.clear()
             for joys in range(pygame.joystick.get_count()):
                 Input.Joystick.Joysticks.append(pygame.joystick.Joystick(joys))
+            
+            if Input.Joystick.controllerCheckingIndex >= 3:
+                Input.Joystick.controllerCheckingIndex = 0
         
         def printJoysticksInfo():
             for joys in Input.Joystick.Joysticks:
-                print(f"ID: {joys.get_instance_id()} - Name: {joys.get_name()} - NumberOfButtons: {joys.get_numbuttons()}")
+                print(f"ID: {joys.get_instance_id()} - Name: {joys.get_name()}")
 
         def Update():
-            #if pygame.CONTROLLERDEVICEADDED:
-            #    Input.Joystick.checkForJoysticks()
-            #    Input.Joystick.printJoysticksInfo()
-                
+            #print(pygame.event.get())
+            try:
+                for device in pygame.event.get(pygame.JOYDEVICEADDED) or pygame.event.get(pygame.JOYDEVICEREMOVED):
+                    if Input.Joystick.controllerCheckingIndex < 4 and device.type == pygame.JOYDEVICEADDED:
+                        Input.Joystick.checkForJoysticks()
+                        Input.Joystick.printJoysticksInfo()
+                        Input.Joystick.controllerCheckingIndex += 1
+                    elif Input.Joystick.controllerCheckingIndex < 4 and device.type == pygame.JOYDEVICEREMOVED:
+                        Input.Joystick.checkForJoysticks()
+                        print("Device Removed")
+                        Input.Joystick.controllerCheckingIndex += 1
+            except:
+                print("-- Error detecting joystick -- Tried 3 Times --")
+
             for joys in Input.Joystick.Joysticks:
-                for joysticks in pygame.event.get(pygame.JOYBUTTONDOWN):
-                    match joysticks.button:
+                for joyButton in pygame.event.get(pygame.JOYBUTTONDOWN):
+                    match joyButton.button:
                         case 0:
-                            print("Controller X")
+                            print(f"Controller {joyButton.joy} X")
                         case 1:
-                            print("Controller Circle")
+                            print(f"Controller {joyButton.joy} Circle")
                         case 2:
-                            print("Controller Triangle")
+                            print(f"Controller {joyButton.joy} Square")
                         case 3:
-                            print("Controller Square")
+                            print(f"Controller {joyButton.joy} Triangle")
                         case 4:
-                            print("Controller L1")
+                            print(f"Controller {joyButton.joy} L1")
                         case 5:
-                            print("Controller R1")
+                            print(f"Controller {joyButton.joy} R1")
+                        case 6:
+                            print(f"Controller {joyButton.joy} Select")
+                        case 7:
+                            print(f"Controller {joyButton.joy} Start")
+                        case 8:
+                            print(f"Controller {joyButton.joy} L3")
+                        case 9:
+                            print(f"Controller {joyButton.joy} R3")
+                        case 10:
+                            print(f"Controller {joyButton.joy} Home")
+                        case 11:
+                            print(f"Controller {joyButton.joy} idk")
                         case _:
                             print("Invalid Button")
+                    
+                for joyHat in pygame.event.get(pygame.JOYHATMOTION):
+                    match joyHat.value:
+                        case (1, 0):
+                            print(f"Controller {joyHat.joy} Hat Right")
+                        case (-1, 0):
+                            print(f"Controller {joyHat.joy} Hat Left")
+                        case (0, 1):
+                            print(f"Controller {joyHat.joy} Hat Up")
+                        case (0, -1):
+                            print(f"Controller {joyHat.joy} Hat Down")
+                
+                for joyAxis in pygame.event.get(pygame.JOYAXISMOTION):
+                    match joyAxis.axis:
+                        case 0:
+                            print(f"Controller {joyAxis.joy} Left Analog V: Value {joyAxis.value}")
+                        case 1:
+                            print(f"Controller {joyAxis.joy} Left Analog H: Value {joyAxis.value}")
+                        case 2:
+                            print(f"Controller {joyAxis.joy} Right Analog V: Value {joyAxis.value}")
+                        case 3:
+                            print(f"Controller {joyAxis.joy} Right Analog H: Value {joyAxis.value}")
+                        case 4:
+                            print(f"Controller {joyAxis.joy} L2: Value {joyAxis.value}")
+                        case 5:
+                            print(f"Controller {joyAxis.joy} R2: Value {joyAxis.value}")
+                
     class Mouse:
         def getMousePosition():
             if pygame.event.peek(pygame.MOUSEMOTION):
